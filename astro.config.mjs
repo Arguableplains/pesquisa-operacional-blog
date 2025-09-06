b39979b1795b49026723774bf76da7f3303c1bbc
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2025-09-06 18:13:44 -0300

    Update Github CICD

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://arguableplains.github.io",
+	base: "/pesquisa-operacional-blog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
