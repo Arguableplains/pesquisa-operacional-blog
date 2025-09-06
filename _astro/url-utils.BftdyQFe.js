fd4fae493d22d4b120fa601df308a1c12ac087a6
Arguableplains <Arguableplains@users.noreply.github.com>
2025-09-06 20:56:45 +0000

    deploy: 431ec11e90052c872b1c9a8cf4602be91b7e6e82

---
+++ _astro/url-utils.BftdyQFe.js
@@ -0,0 +1,1 @@
+import"./pt_br.CflUL2PS.js";function u(n,o){const t=n.replace(/^\/|\/$/g,"").toLowerCase(),e=o.replace(/^\/|\/$/g,"").toLowerCase();return t===e}function r(...n){return n.join("/").replace(/\/+/g,"/")}function i(n){return a(`/posts/${n}/`)}function a(n){return r("","/",n)}export{i as g,u as p,a as u};
